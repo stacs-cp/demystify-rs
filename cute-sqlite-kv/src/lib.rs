61c3da87582270bdabead7ccbc964bdbadecdced
Chris Jefferson <caj21@st-andrews.ac.uk>
2024-06-03 14:25:25 +0800

    Rename and cleanup

--- cute-sqlite-kv/src/lib.rs
+++ cute-sqlite-kv/src/lib.rs
@@ -1,84 +1,85 @@
-/// This crate provides a simple key-value cache implementation using SQLite as the underlying storage.
+/// This crate provides a simple multi-process key-value store,
+///  using SQLite as the underlying storage.
 ///
 /// This code is intended to be as simple as a wrapper around
 /// sqlite (via rusqlite) as possible, while ensuring the same
-/// cache can be used from multiple processes correctly.
+/// KVStore can be used from multiple processes correctly.
 ///
 /// # Examples
 ///
 /// ```
-/// use cute_sqlite_kv::Cache;
+/// use cute_sqlite_kv::KVStore;
 ///
-/// // Create a new in-memory cache
-/// let cache = Cache::new_in_memory().unwrap();
+/// // Create a new in-memory store
+/// let kvstore = KVStore::new_in_memory().unwrap();
 ///
 /// // Set a key-value pair
-/// cache.set("key", "value").unwrap();
+/// kvstore.set("key", "value").unwrap();
 ///
 /// // Get the value for a key
-/// let result = cache.get("key").unwrap();
+/// let result = kvstore.get("key").unwrap();
 /// assert_eq!(result, Some("value".to_string()));
 ///
 /// // Delete a key
-/// cache.delete("key").unwrap();
+/// kvstore.delete("key").unwrap();
 ///
 /// // Check if the key is deleted
-/// let result = cache.get("key").unwrap();
+/// let result = kvstore.get("key").unwrap();
 /// assert_eq!(result, None);
 /// ```
 ///
 /// # Usage
 ///
-/// To use the `Cache` struct, you need to import the `cute_sqlite_kv` crate and create a new cache instance.
-/// You can create a cache either in-memory or from a file.
+/// To use the `KVStore` struct, you need to import the `cute_sqlite_kv` crate and create a new KVStore instance.
+/// You can create a KVStore either in-memory or from a file.
 ///
-/// ## In-Memory Cache
+/// ## In-Memory KVStore
 ///
-/// To create a new in-memory cache, use the `new_in_memory` method:
+/// To create a new in-memory KVStore, use the `new_in_memory` method:
 ///
 /// ```rust
-/// use cute_sqlite_kv::Cache;
+/// use cute_sqlite_kv::KVStore;
 ///
-/// let cache = Cache::new_in_memory().unwrap();
+/// let kvstore = KVStore::new_in_memory().unwrap();
 /// ```
 ///
-/// ## File-based Cache
+/// ## File-based KVStore
 ///
-/// To create a new cache using a file as the storage, use the `new_from_file` method and provide the path to the file:
+/// To create a new KVStore using a file as the storage, use the `new_from_file` method and provide the path to the file:
 ///
 /// ```rust
-/// use cute_sqlite_kv::Cache;
+/// use cute_sqlite_kv::KVStore;
 /// use std::path::Path;
 ///
-/// let filename = Path::new("/tmp/cache.db");
-/// let cache = Cache::new_from_file(filename).unwrap();
+/// let filename = Path::new("/tmp/kvstore.db");
+/// let kvstore = KVStore::new_from_file(filename).unwrap();
 /// ```
 ///
 /// # Methods
 ///
-/// The `Cache` struct provides the following methods:
+/// The `KVStore` struct provides the following methods:
 ///
-/// - `set`: Sets a key-value pair in the cache.
-/// - `get`: Retrieves the value for a given key from the cache.
-/// - `delete`: Deletes a key-value pair from the cache.
+/// - `set`: Sets a key-value pair in the `KVStore`.
+/// - `get`: Retrieves the value for a given key from the `KVStore`.
+/// - `delete`: Deletes a key-value pair from the `KVStore`.
 ///
 /// Please refer to the method documentation for more details on how to use each method.
 ///
 /// # Examples
 ///
 /// ```rust
-/// use cute_sqlite_kv::Cache;
+/// use cute_sqlite_kv::KVStore;
 ///
-/// let cache = Cache::new_in_memory().unwrap();
+/// let kvstore = KVStore::new_in_memory().unwrap();
 ///
-/// cache.set("key", "value").unwrap();
+/// kvstore.set("key", "value").unwrap();
 ///
-/// let result = cache.get("key").unwrap();
+/// let result = kvstore.get("key").unwrap();
 /// assert_eq!(result, Some("value".to_string()));
 ///
-/// cache.delete("key").unwrap();
+/// kvstore.delete("key").unwrap();
 ///
-/// let result = cache.get("key").unwrap();
+/// let result = kvstore.get("key").unwrap();
 /// assert_eq!(result, None);
 /// ```
 ///
@@ -86,54 +87,58 @@ use std::path::Path;
 
 use rusqlite::Connection;
 
-const KEY_COLUMN: &str = "cache_key";
-const VAL_COLUMN: &str = "cache_val";
-const TABLE: &str = "cache_table";
+const KEY_COLUMN: &str = "KVStore_key";
+const VAL_COLUMN: &str = "KVStore_val";
+const TABLE: &str = "KVStore_table";
 
-pub struct Cache {
+pub struct KVStore {
     connection: Connection,
 }
 
-impl Cache {
-    /// Creates a new in-memory cache.
+impl KVStore {
+    /// Creates a new in-memory key-value store.
+    ///
+    /// An in-memory key-value store is in practice worse than
+    /// a standard HashMap in every way, so the only use of this function
+    /// is for creating a key value store for testing.
     ///
     /// # Examples
     ///
     /// ```rust
-    /// use cute_sqlite_kv::Cache;
+    /// use cute_sqlite_kv::KVStore;
     ///
-    /// let cache = Cache::new_in_memory().unwrap();
+    /// let kvstore = KVStore::new_in_memory().unwrap();
     /// ```
-    pub fn new_in_memory() -> rusqlite::Result<Cache> {
+    pub fn new_in_memory() -> rusqlite::Result<KVStore> {
         let connection = Connection::open_in_memory()?;
-        let cache = Cache { connection };
-        cache.create_table()?;
-        Ok(cache)
+        let kvstore = KVStore { connection };
+        kvstore.create_table()?;
+        Ok(kvstore)
     }
 
-    /// Creates a new cache using a file as the storage.
+    /// Creates a new KVStore using a file as the storage.
     ///
     /// # Arguments
     ///
-    /// * `filename` - The path to the file used as the storage for the cache.
+    /// * `filename` - The path to the file used as the storage for the KVStore.
     ///
     /// # Examples
     ///
     /// ```rust
-    /// use cute_sqlite_kv::Cache;
+    /// use cute_sqlite_kv::KVStore;
     /// use std::path::Path;
     ///
-    /// let filename = Path::new("/tmp/cache.db");
-    /// let cache = Cache::new_from_file(filename).unwrap();
+    /// let filename = Path::new("/tmp/kvstore.db");
+    /// let kvstore = KVStore::new_from_file(filename).unwrap();
     /// ```
-    pub fn new_from_file(filename: &Path) -> rusqlite::Result<Cache> {
+    pub fn new_from_file(filename: &Path) -> rusqlite::Result<KVStore> {
         let connection = Connection::open(filename)?;
-        let cache = Cache { connection };
-        cache.create_table()?;
-        Ok(cache)
+        let kvstore = KVStore { connection };
+        kvstore.create_table()?;
+        Ok(kvstore)
     }
 
-    /// Internal function which ensures cache
+    /// Internal function which ensures KVStore
     /// table is created
     fn create_table(&self) -> rusqlite::Result<()> {
         self.connection.execute(
@@ -149,7 +154,7 @@ impl Cache {
         Ok(())
     }
 
-    /// Sets a key-value pair in the cache.
+    /// Sets a key-value pair in the KVStore.
     /// Overwrites any existing value
     ///
     /// # Arguments
@@ -160,11 +165,11 @@ impl Cache {
     /// # Examples
     ///
     /// ```rust
-    /// use cute_sqlite_kv::Cache;
+    /// use cute_sqlite_kv::KVStore;
     ///
-    /// let cache = Cache::new_in_memory().unwrap();
+    /// let kvstore = KVStore::new_in_memory().unwrap();
     ///
-    /// cache.set("key", "value").unwrap();
+    /// kvstore.set("key", "value").unwrap();
     /// ```
     pub fn set(&self, key: &str, value: &str) -> rusqlite::Result<()> {
         self.connection.execute(
@@ -177,7 +182,7 @@ impl Cache {
         Ok(())
     }
 
-    /// Retrieves the value for a given key from the cache.
+    /// Retrieves the value for a given key from the KVStore.
     ///
     /// # Arguments
     ///
@@ -186,13 +191,13 @@ impl Cache {
     /// # Examples
     ///
     /// ```rust
-    /// use cute_sqlite_kv::Cache;
+    /// use cute_sqlite_kv::KVStore;
     ///
-    /// let cache = Cache::new_in_memory().unwrap();
+    /// let kvstore = KVStore::new_in_memory().unwrap();
     ///
-    /// cache.set("key", "value").unwrap();
+    /// kvstore.set("key", "value").unwrap();
     ///
-    /// let result = cache.get("key").unwrap();
+    /// let result = kvstore.get("key").unwrap();
     /// assert_eq!(result, Some("value".to_string()));
     /// ```
 
@@ -210,7 +215,7 @@ impl Cache {
         }
     }
 
-    /// Deletes a key-value pair from the cache,
+    /// Deletes a key-value pair from the KVStore,
     /// if present
     ///
     /// # Arguments
@@ -220,15 +225,15 @@ impl Cache {
     /// # Examples
     ///
     /// ```rust
-    /// use cute_sqlite_kv::Cache;
+    /// use cute_sqlite_kv::KVStore;
     ///
-    /// let cache = Cache::new_in_memory().unwrap();
+    /// let kvstore = KVStore::new_in_memory().unwrap();
     ///
-    /// cache.set("key", "value").unwrap();
+    /// kvstore.set("key", "value").unwrap();
     ///
-    /// cache.delete("key").unwrap();
+    /// kvstore.delete("key").unwrap();
     ///
-    /// let result = cache.get("key").unwrap();
+    /// let result = kvstore.get("key").unwrap();
     /// assert_eq!(result, None);
     /// ```
     pub fn delete(&self, key: &str) -> rusqlite::Result<()> {
@@ -248,117 +253,117 @@ mod tests {
 
     #[test]
     fn test_new_in_memory() {
-        let _ = Cache::new_in_memory().unwrap();
+        let _ = KVStore::new_in_memory().unwrap();
     }
 
     #[test]
     fn test_new_from_file() {
         let temp_dir = tempdir().expect("Failed to create temp directory");
-        let filename = temp_dir.path().join("cache.db");
-        let _ = Cache::new_from_file(&filename).unwrap();
+        let filename = temp_dir.path().join("kvstore.db");
+        let _ = KVStore::new_from_file(&filename).unwrap();
     }
 
     #[test]
     fn test_new_from_file_more() {
         let temp_dir = tempdir().expect("Failed to create temp directory");
-        let filename = temp_dir.path().join("cache.db");
-        let cache = Cache::new_from_file(&filename).unwrap();
+        let filename = temp_dir.path().join("kvstore.db");
+        let kvstore = KVStore::new_from_file(&filename).unwrap();
         let key = "test_key";
         let value = "test_value";
-        cache.set(key, value).unwrap();
-        let result = cache.get(key).unwrap();
+        kvstore.set(key, value).unwrap();
+        let result = kvstore.get(key).unwrap();
         assert_eq!(result, Some(value.to_string()));
     }
 
     #[test]
     fn test_reopen_database() {
         let temp_dir = tempfile::tempdir().expect("Failed to create temp directory");
-        let filename = temp_dir.path().join("cache.db");
+        let filename = temp_dir.path().join("kvstore.db");
         {
-            let cache = Cache::new_from_file(&filename).unwrap();
+            let kvstore = KVStore::new_from_file(&filename).unwrap();
             let key = "test_key";
             let value = "test_value";
-            cache.set(key, value).unwrap();
+            kvstore.set(key, value).unwrap();
         }
         {
-            let cache = Cache::new_from_file(&filename).unwrap();
+            let kvstore = KVStore::new_from_file(&filename).unwrap();
             let key = "test_key";
-            let result = cache.get(key).unwrap();
+            let result = kvstore.get(key).unwrap();
             assert_eq!(result, Some("test_value".to_string()));
         }
     }
 
     #[test]
     fn test_set_and_get() {
-        let cache = Cache::new_in_memory().unwrap();
+        let kvstore = KVStore::new_in_memory().unwrap();
         let key = "test_key";
         let value = "test_value";
-        cache.set(key, value).unwrap();
-        let result = cache.get(key).unwrap();
+        kvstore.set(key, value).unwrap();
+        let result = kvstore.get(key).unwrap();
         assert_eq!(result, Some(value.to_string()));
     }
 
     #[test]
     fn test_get_nonexistent_key() {
-        let cache = Cache::new_in_memory().unwrap();
+        let kvstore = KVStore::new_in_memory().unwrap();
         let key = "nonexistent_key";
-        let result = cache.get(key).unwrap();
+        let result = kvstore.get(key).unwrap();
         assert_eq!(result, None);
     }
 
     #[test]
     fn test_delete() {
-        let cache = Cache::new_in_memory().unwrap();
+        let kvstore = KVStore::new_in_memory().unwrap();
         let key = "test_key";
         let value = "test_value";
-        cache.set(key, value).unwrap();
-        cache.delete(key).unwrap();
-        let result = cache.get(key).unwrap();
+        kvstore.set(key, value).unwrap();
+        kvstore.delete(key).unwrap();
+        let result = kvstore.get(key).unwrap();
         assert_eq!(result, None);
     }
 
     #[test]
     fn test_delete_nonexistent_key() {
-        let cache = Cache::new_in_memory().unwrap();
+        let kvstore = KVStore::new_in_memory().unwrap();
         let key = "nonexistent_key";
-        cache.delete(key).unwrap();
-        let result = cache.get(key).unwrap();
+        kvstore.delete(key).unwrap();
+        let result = kvstore.get(key).unwrap();
         assert_eq!(result, None);
     }
 
     #[test]
     fn test_many_connections() {
         let temp_dir = tempfile::tempdir().expect("Failed to create temp directory");
-        let filename = temp_dir.path().join("cache.db");
+        let filename = temp_dir.path().join("kvstore.db");
 
         // Create the first connection and add a key
         {
-            let cache = Cache::new_from_file(&filename).unwrap();
+            let kvstore = KVStore::new_from_file(&filename).unwrap();
             let key = "test_key";
             let value = "test_value";
-            cache.set(key, value).unwrap();
+            kvstore.set(key, value).unwrap();
         }
 
         // Check if the key is there
         {
-            let cache = Cache::new_from_file(&filename).unwrap();
+            let kvstore = KVStore::new_from_file(&filename).unwrap();
             let key = "test_key";
-            let result = cache.get(key).unwrap();
+            let result = kvstore.get(key).unwrap();
             assert_eq!(result, Some("test_value".to_string()));
         }
 
         // Delete the key
         {
-            let cache = Cache::new_from_file(&filename).unwrap();
+            let kvstore = KVStore::new_from_file(&filename).unwrap();
             let key = "test_key";
-            cache.delete(key).unwrap();
+            kvstore.delete(key).unwrap();
         }
 
         // Check if the key is gone
         {
-            let cache = Cache::new_from_file(&filename).unwrap();
+            let kvstore = KVStore::new_from_file(&filename).unwrap();
             let key = "test_key";
-            let result = cache.get(key).unwrap();
+            let result = kvstore.get(key).unwrap();
             assert_eq!(result, None);
         }
     }
@@ -366,58 +371,58 @@ mod tests {
     #[test]
     fn test_overlapping_connections() {
         let temp_dir = tempfile::tempdir().expect("Failed to create temp directory");
-        let filename = temp_dir.path().join("cache.db");
+        let filename = temp_dir.path().join("kvstore.db");
 
-        let cache = Cache::new_from_file(&filename).unwrap();
+        let kvstore = KVStore::new_from_file(&filename).unwrap();
 
         // Create the first connection and add a key
         {
             let key = "test_key";
             let value = "test_value";
-            cache.set(key, value).unwrap();
+            kvstore.set(key, value).unwrap();
         }
 
-        let cache2 = Cache::new_from_file(&filename).unwrap();
+        let kvstore2 = KVStore::new_from_file(&filename).unwrap();
 
         // Check if the key is there
         {
             let key = "test_key";
-            let result = cache2.get(key).unwrap();
+            let result = kvstore2.get(key).unwrap();
             assert_eq!(result, Some("test_value".to_string()));
         }
 
         // Delete the key
         {
             let key = "test_key";
-            cache2.delete(key).unwrap();
+            kvstore2.delete(key).unwrap();
         }
 
         // Check if the key is gone
         {
             let key = "test_key";
-            let result = cache.get(key).unwrap();
+            let result = kvstore.get(key).unwrap();
             assert_eq!(result, None);
         }
     }
 
     #[test]
     fn test_insert_multiple_times() {
-        let cache = Cache::new_in_memory().unwrap();
+        let kvstore = KVStore::new_in_memory().unwrap();
         let key = "test_key";
         let value1 = "test_value1";
         let value2 = "test_value2";
         let value3 = "test_value3";
 
-        cache.set(key, value1).unwrap();
-        let result1 = cache.get(key).unwrap();
+        kvstore.set(key, value1).unwrap();
+        let result1 = kvstore.get(key).unwrap();
         assert_eq!(result1, Some(value1.to_string()));
 
-        cache.set(key, value2).unwrap();
-        let result2 = cache.get(key).unwrap();
+        kvstore.set(key, value2).unwrap();
+        let result2 = kvstore.get(key).unwrap();
         assert_eq!(result2, Some(value2.to_string()));
 
-        cache.set(key, value3).unwrap();
-        let result3 = cache.get(key).unwrap();
+        kvstore.set(key, value3).unwrap();
+        let result3 = kvstore.get(key).unwrap();
         assert_eq!(result3, Some(value3.to_string()));
     }
 }
