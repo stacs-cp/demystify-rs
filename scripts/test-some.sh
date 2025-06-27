d3af86ec2d1d5c56f7c102fe068d0c3a76313929
Chris Jefferson <caj21@st-andrews.ac.uk>
2025-06-27 15:56:55 +0800

    Run some basic checks to make sure demystify runs

---
+++ scripts/test-some.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -eux
+
+cargo run --release --bin main -- --model eprime/sudoku.eprime --param eprime/sudoku/puzzlingexample.param --merge 5 > /dev/null
+cargo run --release --bin main -- --model eprime/sudoku.eprime --param eprime/sudoku/puzzlingexample.param --merge 5 --html > /dev/null
+cargo run --release --bin main -- --model eprime/loopy.essence --param eprime/loopy/loopy-01.param --merge 2 > /dev/null
+cargo run --release --bin main -- --model eprime/loopy.essence --param eprime/loopy/loopy-01.param --merge 2 --html > /dev/null
