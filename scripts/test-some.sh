37196ed40b7c51579c42fcf90b48afe403d50bbb
Christopher Jefferson <chris@bubblescope.net>
2025-06-30 18:36:45 +0800

    Disable loopy test, as it doesn't work on current savilerow

--- scripts/test-some.sh
+++ scripts/test-some.sh
@@ -4,5 +4,8 @@ set -eux
 
 cargo run --release --bin main -- --model eprime/sudoku.eprime --param eprime/sudoku/puzzlingexample.param --merge 5 > /dev/null
 cargo run --release --bin main -- --model eprime/sudoku.eprime --param eprime/sudoku/puzzlingexample.param --merge 5 --html > /dev/null
-cargo run --release --bin main -- --model eprime/loopy.essence --param eprime/loopy/loopy-01.param --merge 2 > /dev/null
-cargo run --release --bin main -- --model eprime/loopy.essence --param eprime/loopy/loopy-01.param --merge 2 --html > /dev/null
+cargo run --release --bin main -- --model eprime/star-battle.eprime --param eprime/star-battle/FATAtalkexample.param --merge 1 > /dev/null
+cargo run --release --bin main -- --model eprime/star-battle.eprime --param eprime/star-battle/FATAtalkexample.param --merge 1 --html > /dev/null
+
+# cargo run --release --bin main -- --model eprime/loopy.essence --param eprime/loopy/loopy-01.param --merge 2 > /dev/null
+# cargo run --release --bin main -- --model eprime/loopy.essence --param eprime/loopy/loopy-01.param --merge 2 --html > /dev/null
